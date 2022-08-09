73fa329ddb5e726b002c871b396d11ccc01bc6cc
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-09 16:09:16 +0300

    Update UI file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import Calculator from './Components/Calculator';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Calculator />
       </div>
-    )
+    );
   }
 }
 
