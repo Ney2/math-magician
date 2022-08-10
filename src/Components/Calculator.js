6d4124c2577842003ae91b279d228750d11a8c54
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-10 21:36:10 +0300

    Update calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,35 +1,51 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-
+import calculate from '../Logic/claculate';
 class Calculator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: '',
+      operation: '',
+      next: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    this.setState((value) => calculate(value, e.target.innerText))
+  }
+
   render() {
+    const { total, operation, next } = this.state;
     return (
       <main>
         <section className="container">
-          <div className="display"><p className="output">0</p></div>
-          <button type="button" className="clear">AC</button>
-          <button type="button" className="plusminus">+/-</button>
-          <button type="button" className="percentage">%</button>
-          <button type="button" className="division">&divide;</button>
-          <button type="button" className="seven">7</button>
-          <button type="button" className="eight">8</button>
-          <button type="button" className="nine">9</button>
-          <button type="button" className="multi">&times;</button>
-          <button type="button" className="four">4</button>
-          <button type="button" className="five">5</button>
-          <button type="button" className="six">6</button>
-          <button type="button" className="minus">-</button>
-          <button type="button" className="one">1</button>
-          <button type="button" className="two">2</button>
-          <button type="button" className="three">3</button>
-          <button type="button" className="plus">+</button>
-          <button type="button" className="zero">0</button>
-          <button type="button" className="point">.</button>
-          <button type="button" className="equal">=</button>
+          <div className="display"><p className="output">{(total || '') + (operation || '') + (next || '') || 0 }</p></div>
+          <button type="button" className="clear" onClick={this.handleClick()}>AC</button>
+          <button type="button" className="plusminus" onClick={this.handleClick()}>+/-</button>
+          <button type="button" className="percentage" onClick={this.handleClick()}>%</button>
+          <button type="button" className="division" onClick={this.handleClick()}>&divide;</button>
+          <button type="button" className="seven" onClick={this.handleClick()}>7</button>
+          <button type="button" className="eight" onClick={this.handleClick()}>8</button>
+          <button type="button" className="nine" onClick={this.handleClick()}>9</button>
+          <button type="button" className="multi" onClick={this.handleClick()}>&times;</button>
+          <button type="button" className="four" onClick={this.handleClick()}>4</button>
+          <button type="button" className="five" onClick={this.handleClick()}>5</button>
+          <button type="button" className="six" onClick={this.handleClick()}>6</button>
+          <button type="button" className="minus" onClick={this.handleClick()}>-</button>
+          <button type="button" className="one" onClick={this.handleClick()}>1</button>
+          <button type="button" className="two" onClick={this.handleClick()}>2</button>
+          <button type="button" className="three" onClick={this.handleClick()}>3</button>
+          <button type="button" className="plus" onClick={this.handleClick()}>+</button>
+          <button type="button" className="zero" onClick={this.handleClick()}>0</button>
+          <button type="button" className="point" onClick={this.handleClick()}>.</button>
+          <button type="button" className="equal" onClick={this.handleClick()}>=</button>
         </section>
       </main>
     );
   }
 }
 
+Calculator.defaultProps = { total: 0, operation: '', next: 0 };
 export default Calculator;
