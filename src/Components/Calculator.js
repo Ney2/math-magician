1a8956ae2d2377a15861dc2b249d7aed19453ec8
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-11 01:48:01 +0300

    Refactor using hooks

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -11,29 +11,29 @@ const Calculator = () => {
 
   return (
     <main>
-        <section className="container">
-          <p className="output">{calc.next || calc.total || 0}</p>
-          <button type="button" className="clear" onClick={handleClick}>AC</button>
-          <button type="button" className="plusminus" onClick={handleClick}>+/-</button>
-          <button type="button" className="percentage" onClick={handleClick}>%</button>
-          <button type="button" className="division" onClick={handleClick}>&divide;</button>
-          <button type="button" className="seven" onClick={handleClick}>7</button>
-          <button type="button" className="eight" onClick={handleClick}>8</button>
-          <button type="button" className="nine" onClick={handleClick}>9</button>
-          <button type="button" className="multi" onClick={handleClick}>x</button>
-          <button type="button" className="four" onClick={handleClick}>4</button>
-          <button type="button" className="five" onClick={handleClick}>5</button>
-          <button type="button" className="six" onClick={handleClick}>6</button>
-          <button type="button" className="minus" onClick={handleClick}>-</button>
-          <button type="button" className="one" onClick={handleClick}>1</button>
-          <button type="button" className="two" onClick={handleClick}>2</button>
-          <button type="button" className="three" onClick={handleClick}>3</button>
-          <button type="button" className="plus" onClick={handleClick}>+</button>
-          <button type="button" className="zero" onClick={handleClick}>0</button>
-          <button type="button" className="point" onClick={handleClick}>.</button>
-          <button type="button" className="equal" onClick={handleClick}>=</button>
-        </section>
-      </main>
+      <section className="container">
+        <p className="output">{calc.next || calc.total || 0}</p>
+        <button type="button" className="clear" onClick={handleClick}>AC</button>
+        <button type="button" className="plusminus" onClick={handleClick}>+/-</button>
+        <button type="button" className="percentage" onClick={handleClick}>%</button>
+        <button type="button" className="division" onClick={handleClick}>&divide;</button>
+        <button type="button" className="seven" onClick={handleClick}>7</button>
+        <button type="button" className="eight" onClick={handleClick}>8</button>
+        <button type="button" className="nine" onClick={handleClick}>9</button>
+        <button type="button" className="multi" onClick={handleClick}>x</button>
+        <button type="button" className="four" onClick={handleClick}>4</button>
+        <button type="button" className="five" onClick={handleClick}>5</button>
+        <button type="button" className="six" onClick={handleClick}>6</button>
+        <button type="button" className="minus" onClick={handleClick}>-</button>
+        <button type="button" className="one" onClick={handleClick}>1</button>
+        <button type="button" className="two" onClick={handleClick}>2</button>
+        <button type="button" className="three" onClick={handleClick}>3</button>
+        <button type="button" className="plus" onClick={handleClick}>+</button>
+        <button type="button" className="zero" onClick={handleClick}>0</button>
+        <button type="button" className="point" onClick={handleClick}>.</button>
+        <button type="button" className="equal" onClick={handleClick}>=</button>
+      </section>
+    </main>
   );
 };
 
