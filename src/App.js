7ff500d0ab79e84441e70a007373730fc6b011bb
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-17 02:35:42 +0300

    Update files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Calculator from './Components/Calculator';
+import List from './Components/List';
+import Home from './Components/Home';
+import Quote from './Components/Quote';
+import Footer from './Components/Footer';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+        <BrowserRouter>
+            <List />
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/calculator' element={<Calculator />}/>
+                <Route path='/quotes' element={<Quote />}/>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
-  }
-}
+};
 
 export default App;
