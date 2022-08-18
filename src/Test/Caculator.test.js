17837ccbdb47465c5101f2ce3299e19c0beb2179
Esther <eudondian@gmaile.com>
2022-08-18 14:24:44 +0100

    Modify Caculator.test.js file

--- src/Test/Caculator.test.js
+++ src/Test/Caculator.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import Calculator from '../Components/Calculator';
 
+// eslint-disable-next-line no-undef
 it('renders correctly', () => {
-    const tree = Renderer
-        .create(<Calculator />)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = Renderer
+    .create(<Calculator />)
+    .toJSON();
+    // eslint-disable-next-line no-undef
+  expect(tree).toMatchSnapshot();
 });
