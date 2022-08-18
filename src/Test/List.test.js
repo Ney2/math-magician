c6ee11821bcd69bfc6d8cc5d9968a60ecd82f60b
Esther <eudondian@gmaile.com>
2022-08-18 14:27:16 +0100

    Modify Liste.test.js file

--- src/Test/List.test.js
+++ src/Test/List.test.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Renderer from 'react-test-renderer';
 import List from '../Components/List';
 
-const MockHeader = () => (
+function MockHeader() {
+  return (
     <BrowserRouter>
-        <List />
+      <List />
     </BrowserRouter>
-);
+  );
+}
 
 it('Renders correctly', () => {
-    const tree = Renderer
-        .create(<MockHeader />)
-        .toJSON();
+  const tree = Renderer
+    .create(<MockHeader />)
+    .toJSON();
 
-    expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
 });
