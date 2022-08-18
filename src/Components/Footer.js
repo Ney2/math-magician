838f98ff190eae84d29961534291cdafd611a3ed
Esther <eudondian@gmaile.com>
2022-08-18 14:36:08 +0100

    Modify linter file

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../Components/CSS/Footer.css';
+import './CSS/Footer.css';
 
-const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className='copyright'>
-                <h4>Designed by Kidist-Guta</h4>
-                <p>Copyright@2022</p>
-            </div>
-        </div>
-    );
-};
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        <h4>Designed by Kidist-Guta</h4>
+        <p>Copyright@2022</p>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
--- src/Logic/operate.test.js
+++ src/Logic/operate.test.js
@@ -1,88 +1,88 @@
 import operate from './operate';
 
 describe('Addition', () => {
-    test('Addition of two positive numbers', () => {
-        const result = operate('1', '2', '+');
-        expect(result).toBe('3');
-    });
-    test('Addition of two negative numbers', () => {
-        const result = operate('-1', '-2', '+');
-        expect(result).toBe('-3');
-    });
-    test('Addition of a positive number and a negative number', () => {
-        const result = operate('1', '-2', '+');
-        expect(result).toBe('-1');       
-    });
+  test('Addition of two positive numbers', () => {
+    const result = operate('1', '2', '+');
+    expect(result).toBe('3');
+  });
+  test('Addition of two negative numbers', () => {
+    const result = operate('-1', '-2', '+');
+    expect(result).toBe('-3');
+  });
+  test('Addition of a positive number and a negative number', () => {
+    const result = operate('1', '-2', '+');
+    expect(result).toBe('-1');
+  });
 });
 
 describe('Subtraction', () => {
-    test('Subtraction of two positive numbers', () => {
-        const result = operate('4', '2', '-');
-        expect(result).toBe('2');
-    });
-    test('Subtraction of two negative numbers', () => {
-        const result = operate('-4', '-2', '-');
-        expect(result).toBe('-2');
-    });
-    test('Subtraction of a negative number and a positive number', () => {
-        const result = operate('-4', '2', '-');
-        expect(result).toBe('-6');       
-    });
+  test('Subtraction of two positive numbers', () => {
+    const result = operate('4', '2', '-');
+    expect(result).toBe('2');
+  });
+  test('Subtraction of two negative numbers', () => {
+    const result = operate('-4', '-2', '-');
+    expect(result).toBe('-2');
+  });
+  test('Subtraction of a negative number and a positive number', () => {
+    const result = operate('-4', '2', '-');
+    expect(result).toBe('-6');
+  });
 });
 
 describe('Multiplication', () => {
-    test('Product of two positive numbers', () => {
-        const result = operate('2', '2', 'x');
-        expect(result).toBe('4');
-    });
-    test('Product of two negative numbers', () => {
-        const result = operate('-2', '-2', 'x');
-        expect(result).toBe('4');
-    });
-    test('Product of a negatiave and a positive number', () => {
-        const result = operate('-2', '2', 'x');
-        expect(result).toBe('-4');
-    });
-    test('Product of a number with 0', () => {
-        const result = operate('0', '2', 'x');
-        expect(result).toBe('0');       
-    });
+  test('Product of two positive numbers', () => {
+    const result = operate('2', '2', 'x');
+    expect(result).toBe('4');
+  });
+  test('Product of two negative numbers', () => {
+    const result = operate('-2', '-2', 'x');
+    expect(result).toBe('4');
+  });
+  test('Product of a negatiave and a positive number', () => {
+    const result = operate('-2', '2', 'x');
+    expect(result).toBe('-4');
+  });
+  test('Product of a number with 0', () => {
+    const result = operate('0', '2', 'x');
+    expect(result).toBe('0');
+  });
 });
 
 describe('Division', () => {
-    test('Division of two postive numbers', () => {
-        const result = operate('4', '2', '÷');
-        expect(result).toBe('2');
-    });
-    test('Division of two negative numbers', () => {
-        const result = operate('-4', '-2', '÷');
-        expect(result).toBe('2');
-    });
-    test('Division of a negative number and a positive number', () => {
-        const result = operate('-4', '2', '÷');
-        expect(result).toBe('-2');
-    });
-    test('Division by 0', () => {
-        const result = operate('2', '0', '÷');
-        expect(result).toBe('Can\'t divide by 0.');       
-    });
+  test('Division of two postive numbers', () => {
+    const result = operate('4', '2', '÷');
+    expect(result).toBe('2');
+  });
+  test('Division of two negative numbers', () => {
+    const result = operate('-4', '-2', '÷');
+    expect(result).toBe('2');
+  });
+  test('Division of a negative number and a positive number', () => {
+    const result = operate('-4', '2', '÷');
+    expect(result).toBe('-2');
+  });
+  test('Division by 0', () => {
+    const result = operate('2', '0', '÷');
+    expect(result).toBe('Can\'t divide by 0.');
+  });
 });
 
 describe('Modulo', () => {
-    test('Modulo of two postivie numbers', () => {
-        const result = operate('4', '3', '%');
-        expect(result).toBe('1');
-    });
-    test('Modulo of two negative numbers', () => {
-        const result = operate('-4', '-3', '%');
-        expect(result).toBe('-1');
-    });
-    test('Modulo of a positive and a negative number', () => {
-        const result = operate('-4', '3', '%');
-        expect(result).toBe('-1');
-    });
-    test('Division by 0', () => {
-        const result = operate('2', '0', '%');
-        expect(result).toBe('Can\'t find modulo as can\'t divide by 0.');       
-    });
+  test('Modulo of two postivie numbers', () => {
+    const result = operate('4', '3', '%');
+    expect(result).toBe('1');
+  });
+  test('Modulo of two negative numbers', () => {
+    const result = operate('-4', '-3', '%');
+    expect(result).toBe('-1');
+  });
+  test('Modulo of a positive and a negative number', () => {
+    const result = operate('-4', '3', '%');
+    expect(result).toBe('-1');
+  });
+  test('Division by 0', () => {
+    const result = operate('2', '0', '%');
+    expect(result).toBe('Can\'t find modulo as can\'t divide by 0.');
+  });
 });
