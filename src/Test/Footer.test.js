9ffe3430d83eb4d4738af13c2bcbf180b8b5c1a1
Esther <eudondian@gmaile.com>
2022-08-18 14:25:24 +0100

    Modify Footer.test.js file

--- src/Test/Footer.test.js
+++ src/Test/Footer.test.js
@@ -3,8 +3,8 @@ import Renderer from 'react-test-renderer';
 import Footer from '../Components/Footer';
 
 it('renders correctly', () => {
-    const tree = Renderer
-        .create(<Footer/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = Renderer
+    .create(<Footer />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
