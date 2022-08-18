61678f5b7226b5f3463949ae2004993c3e6e0964
Esther <eudondian@gmaile.com>
2022-08-18 14:34:27 +0100

    Modify Quote.test.js file

--- src/Test/Quote.test.js
+++ src/Test/Quote.test.js
@@ -3,8 +3,8 @@ import Renderer from 'react-test-renderer';
 import Quote from '../Components/Quote';
 
 it('renders correctly', () => {
-    const tree = Renderer
-        .create(<Quote/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = Renderer
+    .create(<Quote />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
